2faac82b4683825ed070813e58ec38a9cc62d013
JSPark <jspark@JSParkui-MacBook-Pro.local>
2017-04-17 16:18:48 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jspark/githubandroidstudio_test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jspark.githubandroidstudio_test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
